396e5ed0e4e7e41e0ae9ecc12ae4a70fbe1f4e8a
Ashish Yadav <secondgentechnerd@gmail.com>
2022-01-26 17:00:41 +0530

    Added Logic Exp

--- DSA/DSA Level - 1/Basics of Programming/2-D Arrays/Exit Point Of A Matrix.cpp
+++ DSA/DSA Level - 1/Basics of Programming/2-D Arrays/Exit Point Of A Matrix.cpp
@@ -1,3 +1,55 @@
+/*
+Matrix will only consist of 0 , 1
+If it encounters 1 turn 90 deg right
+
+
+n = 4, m = 4
+
+0 0 1 0
+1 0 0 0
+0 0 0 0
+1 0 1 0
+
+Output = 1 3
+
+Explanation :
+
+0 > 0 > 1
+Logic : Basically we have two increase and decrease
+value of i and j indexes as per direction 
+(East = 0, South = 1, West = 2, North = 3 )
+it'll travesrse head on if  val is 0 else turn right
+So,       East > South > West > North
+if val 1 is encountered simply increase dir by 1
+if dir = 0 ,    i++ 
+if dir = 1 ,    j++
+if dir = 2 ,    i--
+if dir = 3 ,    j--
+dir = (dir + arr[i][j]) % 4 : Modulo 4 is because dir
+                              will be from 0 - 3
+
+
+
+D-R-Y Run Of Above Testcase
+i j dir
+0 0 0
+0 1 0
+0 2 1
+1 2 1
+2 2 1
+3 2 2
+3 1 2
+3 0 3
+2 0 3
+1 0 2
+1 1 2
+1 2 2
+1 3 3 -> Exit Point 
+
+*/
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
