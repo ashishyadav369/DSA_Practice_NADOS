bac447679564ef3eb7c5ac46f610f12c80917494
Ashish Yadav <secondgentechnerd@gmail.com>
2022-01-28 13:49:46 +0530

    Added Optimized Sol

--- DSA/DSA Level - 1/Basics of Programming/2-D Arrays/Search in a Sorted Array.cpp
+++ DSA/DSA Level - 1/Basics of Programming/2-D Arrays/Search in a Sorted Array.cpp
@@ -14,8 +14,8 @@ Output : 3 2
 #include <vector>
 using namespace std;
 
-bool searchMatrix(vector<vector<int> >& matrix, int target) 
-{
+bool searchMatrix(vector<vector<int> >& matrix, int target) {
+    // T = O(n^2)
     for(int i = 0; i < matrix.size(); i++) {
         for(int j = 0; j < matrix[i].size(); j++) {
             if(matrix[i][j] == target) {
@@ -23,6 +23,22 @@ bool searchMatrix(vector<vector<int> >& matrix, int target)
                 return true;
             }
         }
+    }
+    return false;
+}
+bool searchMat(vector<vector<int> >& matrix, int target) {
+    // T = O(N + M) if n == m then O(2n)
+    int i = 0;
+    int j = matrix.size() - 1;
+    while(i < matrix.size() && j >= 0) {
+        if(target == matrix[i][j]){
+            cout << i << endl << j;
+            return true;
+        }
+        else if(target > matrix[i][j])
+            i++;
+        else if(target < matrix[i][j])
+            j--;
     }
     return false;
 }
@@ -36,6 +52,5 @@ int main()
         for(int j = 0; j < n; j++)
             cin>>mat[i][j];
     cin>>target;
-
-    if(!searchMatrix(mat, target)) cout<<"Not Found\n";
+    if(!searchMat(mat, target)) cout<<"Not Found\n";
 }
